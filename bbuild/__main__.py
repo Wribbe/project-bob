72aa8b7e55a80868e2818e370694c9677528c94d
Stefan Eng <eng_steff@outlook.com>
2022-02-08 20:33:09 +0100

    feat: Don't run development mode as default entry-point.

--- bbuild/__main__.py
+++ bbuild/__main__.py
@@ -1,6 +1,10 @@
 import os
 from bbuild.app import app
 
-def run():
+def run_dev():
     os.environ['FLASK_ENV'] = 'development'
     app.run('0.0.0.0', debug=True)
+
+
+def run():
+    app.run('0.0.0.0')
