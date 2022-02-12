8b4f64ee17213362f526351b39025a594d6a050f
Stefan Eng <eng_steff@outlook.com>
2022-02-12 22:43:17 +0100

    feat: Bind api blueprint to api.* subdomain.

--- bbuild/app.py
+++ bbuild/app.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask, render_template
 from bbuild.api import api as api_blueprint
 
-app = Flask(__name__)
-app.register_blueprint(api_blueprint, url_prefix="/api")
+app = Flask(__name__, subdomain_matching=True)
+app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME', 'localhost:5000')
+app.register_blueprint(api_blueprint, subdomain="api")
 
 
 @app.route('/')
