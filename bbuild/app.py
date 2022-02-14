ccd77533512a30205be0962b56585c9910ce181c
Stefan Eng <eng_steff@outlook.com>
2022-02-14 22:21:31 +0100

    feat: Use api to create new repository via index form.

--- bbuild/app.py
+++ bbuild/app.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from bbuild.api import api as api_blueprint
 
 app = Flask(__name__, subdomain_matching=True)
@@ -8,6 +9,15 @@ app.config['SERVER_NAME'] = os.getenv('SERVER_NAME', 'localhost:5000')
 app.register_blueprint(api_blueprint, subdomain="api")
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+
+    if request.method == "POST":
+        payload = request.form.to_dict()
+        resp = requests.post(
+            url_for('api.repos_route'),
+            json=payload,
+        )
+        return redirect(url_for('index'))
+
     return render_template('index.html')
