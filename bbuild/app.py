c5bd7cb367b7a4b393bcc935854b0c9a6c78a8e3
Stefan Eng <eng_steff@outlook.com>
2022-02-14 22:14:33 +0100

    ref: Use os.getenv in place of os.environ.get.

--- bbuild/app.py
+++ bbuild/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from bbuild.api import api as api_blueprint
 
 app = Flask(__name__, subdomain_matching=True)
-app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME', 'localhost:5000')
+app.config['SERVER_NAME'] = os.getenv('SERVER_NAME', 'localhost:5000')
 app.register_blueprint(api_blueprint, subdomain="api")
 
 
