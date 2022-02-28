cd1212a83d168d221b2d4e5a67bffc6c50b173db
Stefan Eng <eng_steff@outlook.com>
2022-02-28 22:15:23 +0100

    build: Explicitly set path env-vars to strings.

--- bbuild/run_prod.py
+++ bbuild/run_prod.py
@@ -3,8 +3,8 @@ from pathlib import Path
 PATH_BBUILD = Path("c:/bbuild")
 
 import os
-os.environ['BBUILD_DATA'] = PATH_BBUILD
-os.environ['BBUILD_REPOS'] = PATH_BBUILD / 'repos'
+os.environ['BBUILD_DATA'] = str(PATH_BBUILD)
+os.environ['BBUILD_REPOS'] = str(PATH_BBUILD / 'repos')
 
 from bbuild.app import app
 app.run('0.0.0.0')
